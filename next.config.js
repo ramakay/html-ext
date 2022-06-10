0fe7966e43d90e4ab53ec6b2b9d831047c56b290
Rama <rannaswamy@childrensplace.com>
2022-06-10 07:37:14 -0400

    init

---
+++ next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  pageExtensions: ['html', 'md', 'jsx', 'js', 'tsx', 'ts'],
+
+}
+
+module.exports = nextConfig
